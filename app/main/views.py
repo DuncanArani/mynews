b5ba6717f7e9b8f36f7d98751b2f31c0753f2f1a
ARUNCO <duncanarani@gmail.com>
2018-10-05 10:15:14 +0300

    ENTERTAINMENT

--- app/main/views.py
+++ app/main/views.py
@@ -19,14 +19,70 @@ def index():
     technology_sources = get_sources('technology')
     entertainment_sources = get_sources('entertainment')
     general_sources = get_sources('general')
+    articles_sources = get_sources('articles')
+    news_sources = get_sources('news')
 
     title = "News Room"
 
-    	# return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
+    return render_template('index.html', title=title, sources=sources, sports_sources=sports_sources, technology_sources=technology_sources, entertainment_sources=entertainment_sources, general_sources=general_sources, article_sources=articles_sources, news_sources=news_sources)
 
 
-    return render_template('index.html', title=title, sources=sources, sports_sources=sports_sources, technology_sources=technology_sources, entertainment_sources=entertainment_sources, general_sources=general_sources)
+@main.route('/sources/<id>')
+def articles(id):
+    '''
+    view articles page
+    '''
+    articles = get_articles(id)
+    title = f'NH | {id}'
 
-    # title = 'Home - Welcome to the number 1 news highlighting website online'
+    return render_template('articles.html', title=title, articles=articles)
 
-    # return render_template('index.html', business=business, sports=sports, technology=technology, entertainment = entertainmment, general=general)
+
+@main.route('/news/<id>')
+def news(id):
+
+    # view root page function that returns the news page and its data
+
+    news = get_news(id)
+
+    return render_template('news.html', news=news_articles)
+
+
+@main.route('/categories/<category>')
+def general(category):
+
+    # view root page function that returns the categories page and its data
+
+    news_categories_articles = get_category(category)
+
+    return render_template('general.html', general=news_categories_articles)
+
+
+@main.route('/categories/<category>')
+def business(category):
+
+    # view root page function that returns the categories page and its data
+
+    news_categories_articles = get_category(category)
+
+    return render_template('business.html', business=news_categories_articles)
+
+
+@main.route('/categories/<category>')
+def entertainment(category):
+
+    # view root page function that returns the categories page and its data
+
+    news_categories_articles = get_category(category)
+
+    return render_template('entertainment.html', entertainment=news_categories_articles)
+
+
+# @main.route('/categories/<category>')
+# def health(category):
+
+#     #view root page function that returns the categories page and its data
+
+#     news_categories_articles = get_category(category)
+
+#     return render_template('health.html', health = news_categories_articles)
